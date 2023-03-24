ebf20cb8fd082ba755a48b131f3d0e17fedb4482
Saloni Verma <saloniv2409@gmail.com>
2023-03-25 01:26:19 +0530

    search complete

--- pages/events/[...eventtime].js
+++ pages/events/[...eventtime].js
@@ -1,6 +1,15 @@
+import EventList from "@/components/events/EventList";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from '../../data/dummy-data';
+
 function FilteredEvents() {
+    const param = useRouter()?.query?.eventtime;
+    const obToSend = {year: +param[0], month: +param[1]}
+    const filteredEv = getFilteredEvents(obToSend);
+
     return ( <>
     <h2>Filtered Events:</h2>
+    <EventList eventList = {filteredEv}/>
     </> );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,17 @@
+import { useRouter } from "next/router";
 import EventList from "@/components/events/EventList";
 import EventsSearch from "@/components/events/EventsSearch";
 import { getAllEvents } from "@/data/dummy-data";
 
 function AllEvents() {
     const allEvents = getAllEvents();
+    const router = useRouter();
+    function findEventsHandler(year, month){
+        const fullPath = `events/${year}/${month}`;
+        router.push(fullPath);
+    }
     return ( <>
-    <EventsSearch/>
+    <EventsSearch findEventsHandler={findEventsHandler}/>
     <h2>Events</h2>
     <EventList eventList={allEvents}/>
     </> );
